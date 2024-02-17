f9041db4d5109e01cc63621625c01696770a4cd4
ShovanTheMan <shovan2192@gmail.com>
2024-02-18 00:03:04 +0600

    typeerror added

---
+++ type-error.js
@@ -0,0 +1,12 @@
+const numbers = 5;
+// numbers.push(45);
+
+let address = {
+    city: 'B-baria',
+    postcode: 4567,
+}
+console.log(address?.home?.building);
+// console.log(address.home.building); this produce error
+
+const a = 'abul';
+
