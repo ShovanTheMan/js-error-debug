f772394b0139b00973a360226dae49b0f4e848b5
ShovanTheMan <shovan2192@gmail.com>
2024-02-18 00:17:10 +0600

    reference added

---
+++ reference-error.js
@@ -0,0 +1,18 @@
+const x = 'pori';
+const y = 'moni';
+
+
+// console.log(add2(3,4));
+
+function add(n1,n2){
+    return n1 + n2;
+}
+const add2 = (n1,n2) => n1 + n2;
+// const add2 = (n1,n2) => n1 + n2;
+// const call korle upore jay na ja kina function use korle upore niche jay kintu const e upore jabe na.
+// const console er age rakle output asbe
+
+function doSomething(){
+    const sum = 55;
+}
+// console.log(sum);
