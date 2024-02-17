e9ad6e522477b8624a831c3f3c127f784787308a
ShovanTheMan <shovan2192@gmail.com>
2024-02-17 23:48:15 +0600

    syntax added

---
+++ syntax-error.js
@@ -0,0 +1,36 @@
+function isBigger(num){
+    if(num > 5){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+const result = isBigger(11);
+// console.log(result);
+
+
+const musk = {
+    name: 'elon',
+    age: 45,
+    job: 'tesla',
+}
+// console.log(musk.age);
+
+const numbers = [34, 65, 76, 34, 65, 76]
+for(let i = 0; i< 69; i++){
+    const n = numbers[i];
+}
+console.log(numbers);
+//if er vitore double condition thakle bracket akta hobe
+
+if (numbers.length > 5 && numbers[0]=== 4){
+
+}
+//error---
+// if (numbers.length > 5) && (numbers[0]=== 4){
+
+// }
+
+
